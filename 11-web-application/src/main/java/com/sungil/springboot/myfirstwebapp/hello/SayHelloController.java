68ea7af9cbc7bd67e4ebe543d542001c46f666f6
Lee-sung-il <wj132612@naver.com>
2026-01-08 13:57:35 +0900

    HTML 응답을 제공학위 위해 Spring MVC 컨트롤러 개선 하기

--- 11-web-application/src/main/java/com/sungil/springboot/myfirstwebapp/hello/SayHelloController.java
+++ 11-web-application/src/main/java/com/sungil/springboot/myfirstwebapp/hello/SayHelloController.java
@@ -1,4 +1,33 @@
 package com.sungil.springboot.myfirstwebapp.hello;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class SayHelloController {
+    // "say-hello" => "Hello! What are you learning today?"
+
+    //"say-hello"
+    //http://localhost:8080/say-hello
+    @RequestMapping("say-hello")
+    @ResponseBody
+    public String sayHello() {
+        return "Hello! What are you learning today?";
+    }
+
+    @RequestMapping("say-hello-html")
+    @ResponseBody
+    public String sayHelloHtml() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("<html>");
+        sb.append("<head>");
+        sb.append("<title>My First HTML Page -Changed</title>");
+        sb.append("</head>");
+        sb.append("<body>");
+        sb.append("My First html page with body - Changed");
+        sb.append("</body>");
+        sb.append("</html>");
+        return sb.toString();
+    }
 }
